6863f957ce9c3ec1c79d877452e79012334d37df
Artem Riabokon <artem.riabokon@pearson.com>
2019-02-14 15:28:07 -0800

    Added two-buckets-one-config sample

---
+++ src/main/java/com/banana/Second.java
@@ -0,0 +1,50 @@
+/*
+ * PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
+ * Copyright (c) 2017 Pearson Education, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property of
+ * Pearson Education, Inc. The intellectual and technical concepts contained
+ * herein are proprietary to Pearson Education, Inc. and may be covered by U.S.
+ * and Foreign Patents, patent applications, and are protected by trade secret
+ * or copyright law. Dissemination of this information, reproduction of this
+ * material, and copying or distribution of this software is strictly forbidden
+ * unless prior written permission is obtained from Pearson Education, Inc.
+
+ */
+package com.banana;
+
+import com.couchbase.client.java.repository.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+
+import static org.springframework.data.couchbase.core.mapping.id.GenerationStrategy.UNIQUE;
+
+@Document(expiryExpression = "300")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Second implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = UNIQUE)
+    @NotEmpty
+    public String id;
+
+    @NotNull
+    public String firstName;
+
+    @NotBlank
+    public String lastName;
+
+    @Min(5)
+    @Max(20)
+    public int activeMinutes;
+}
--- src/main/java/com/banana/SpringBananaApplication.java
+++ src/main/java/com/banana/SpringBananaApplication.java
@@ -2,20 +2,15 @@ package com.banana;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Signal;
-import reactor.util.context.Context;
 
-import java.util.Optional;
-import java.util.function.Consumer;
+import java.util.UUID;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
@@ -27,48 +22,40 @@ public class SpringBananaApplication {
         SpringApplication.run(SpringBananaApplication.class, args);
     }
 
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    SecondRepository secondRepo;
+
     @Bean
     public RouterFunction<ServerResponse> router() {
 
-        log.info("0");
 
         return
                 RouterFunctions
 
                         //Return a flux stream that breaks on element #4, as defined in Line constructor
-                        .route(GET("/flux"), serverRequest -> {
-                            Flux<String> lineFlux = Flux.just("1")
-                                    .doOnNext(l -> {
-                                        log.info("2");
-                                    })
-                                    .subscriberContext(c -> c.putAll(Context.of("time", System.currentTimeMillis(), "xxx", "xxx")));
-                            ;
+                        .route(GET("/save1"), serverRequest -> {
+                            User u = new User(UUID.randomUUID().toString(), "FIRST", "LAST", 10);
 
+                            return ServerResponse.ok().body(userRepo.save(u), User.class);
+                        })
+                        .andRoute(GET("/save2"), serverRequest -> {
+                            Second s = new Second(UUID.randomUUID().toString(), "FIRST", "LAST", 10);
+                            return ServerResponse.ok().body(secondRepo.save(s), Second.class);
+                        })
 
-                            return ServerResponse
-                                    .ok()
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(lineFlux, String.class);
+                        .andRoute(GET("/users"), serverRequest -> {
+                            return ServerResponse.ok()
+                                    .body(userRepo.findAll(), User.class);
+                        })
+
+                        .andRoute(GET("/second"), serverRequest -> {
+                            return ServerResponse.ok()
+                                    .body(secondRepo.findById("0bea7640-ea9c-4d42-a63b-fde529cab196"), Second.class);
                         });
     }
 
-    //This one works in .doOnNext()
-    private static <T> Consumer<Signal<T>> logOnNext(Consumer<T> logStatement) {
-        return signal -> {
-
-            if (!signal.isOnNext()) {
-                return;
-            }
-
-            Optional<String> valueFromContext = signal.getContext().getOrEmpty("xxx");
-            if (valueFromContext.isPresent()) {
-                try (MDC.MDCCloseable closeable = MDC.putCloseable("xxx", valueFromContext.get())) {
-                    logStatement.accept(signal.get());
-                }
-            } else {
-                logStatement.accept(signal.get());
-            }
-        };
-    }
 
 }
