02745fcbf2ccaa2c9f516c9eed13dfe2fe53c7fd
Artem Riabokon <artem.riabokon@pearson.com>
2020-02-11 14:54:40 -0800

    Added a Duration property sample

--- src/main/java/com/banana/SpringBananaApplication.java
+++ src/main/java/com/banana/SpringBananaApplication.java
@@ -3,6 +3,7 @@ package com.banana;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,10 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import java.time.Duration;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
@@ -26,13 +30,8 @@ public class SpringBananaApplication {
         SpringApplication.run(SpringBananaApplication.class, args);
     }
 
-    @Autowired
-    WebClient client;
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.create("http://localhost:8080/");
-    }
+    @Value("${banana.time:100ms}")
+    Duration bananaTime;
 
     @Bean
     public RouterFunction<ServerResponse> router() {
@@ -40,78 +39,11 @@ public class SpringBananaApplication {
                 RouterFunctions
 
                         //Return a flux stream that breaks on element #4, as defined in Line constructor
-                        .route(GET("/flux"), serverRequest -> {
-                            Flux<Line> lineFlux = Flux.just("1", "2", "3", "4", "5")
-                                    .delayElements(Duration.ofSeconds(1))
-                                    .map(Object::toString)
-                                    .map(Line::new)
-//                                    .onErrorContinue((err, ojb) -> System.err.println("Continued on /flux"))
-                                    .log();
-
-
-                            return ServerResponse
-                                    .ok()
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(lineFlux, Line.class);
-                        })
-
-                        //Does a WebClient GET request to /flux above
-                        .andRoute(GET("/getFlux"), serverRequest -> {
-
-                            Flux<Line> resp = client.get()
-                                    .uri("/flux")
-                                    .accept(MediaType.APPLICATION_STREAM_JSON)
-                                    .retrieve()
-                                    .bodyToFlux(Line.class)
-                                    .doOnNext(l -> log.info("RECV : " + l));
-
-                            return ServerResponse
-                                    .ok()
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(resp, Line.class);
-                        })
-
-
-                        //--------------------------------------------
-
-                        //Receives a flux body
-                        .andRoute(POST("/flux"), serverRequest -> {
-
-                            Flux<Line> response = serverRequest
-                                    .bodyToFlux(Line.class)
-                                    .map(l -> new Line(l.value + "-PROCESSED"))
-                                    .doOnNext(l -> log.info("PROCESSED : " + l));
-
-                            return ServerResponse
-                                    .ok()
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(response, Line.class);
-                        })
-
-                        //Sends a Flux body
-                        .andRoute(GET("/postFlux"), serverRequest -> {
-
-
-                            Flux<Line> lineFlux = Flux.just("1", "2", "3")
-                                    .delayElements(Duration.ofSeconds(2))
-                                    .map(Line::new);
-
-                            Flux<Line> resp = client.post()
-                                    .uri("/flux")
-                                    .accept(MediaType.APPLICATION_STREAM_JSON)
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(lineFlux, Line.class)
-                                    .retrieve()
-                                    .bodyToFlux(Line.class)
-                                    .doOnNext(l -> log.info("RECV : " + l));
-
-                            return ServerResponse
-                                    .ok()
-                                    .contentType(MediaType.APPLICATION_STREAM_JSON)
-                                    .body(resp, Line.class);
-                        })
-
-                ;
+                        .route(GET("/time"), serverRequest ->
+                                ServerResponse.ok()
+                                        .body(Mono.just("response" + bananaTime).delayElement(bananaTime), String.class));
     }
 
 }
+
+
